5ee1b7a9c9ab272f6064c95993f99fd4694795bb
Adam Iannazzone <adam@iannaz.zone>
2021-02-05 13:47:39 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Adam Iannazzone on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
